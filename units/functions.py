3604263966810b104ccadb5625b76995446578d1
stuchalk <schalk@unf.edu>
2025-12-22 17:20:43 -0500

    Removed static files related to ingesting/updating data. The functions have been moved to the umis updater app (push 2)

--- units/functions.py
+++
@@ -1,9 +0,0 @@
-from pytz import timezone
-from datetime import datetime
-
-
-def getds():
-    est = timezone('US/Eastern')
-    local = est.localize(datetime.now())
-    fmt = '%Y-%m-%d %H:%M:%S %z'
-    return local.strftime(fmt)
