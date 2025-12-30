fffe1b474ba6659c64966e7710b2fb708f97761a
stuchalk <schalk@unf.edu>
2025-12-30 11:15:04 -0500

    removal of dashboard app

--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-""" urls for the dashboard app """
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("", views.overview, name='overview'),
-    path("dashajax", views.dashajax, name='ajax'),
-]
